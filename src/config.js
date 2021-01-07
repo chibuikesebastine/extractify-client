68d196be6cdc2176374ee9291eec4aa1925fb600
chibuikesebastine <chibuike@kudi.com>
2021-01-08 00:04:11 +0100

    Updated service url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 
-export const extractifyBaseUrl = "http://localhost:5001/extractify-5efd7/us-central1/api"
+export const extractifyBaseUrl = "https://us-central1-extractify-5efd7.cloudfunctions.net/api"
